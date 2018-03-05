cf8f3e558b51ac6c2fc987919343f5e4069a3e0d
Gwen Au <quyen_au@hotmail.com>
2018-03-05 15:10:55 +1100

    Install MongoDB packages

--- server.js
+++ server.js
@@ -3,19 +3,29 @@
 const Path = require('path');
 const Hapi = require('hapi');
 const Inert = require('inert');
-
-const server = new Hapi.Server({
-    port: 3000,
-    routes: {
-        files: {
-            relativeTo: Path.join(__dirname, 'public')
-        }
-    }
-});
+const Boom = require('boom');
+const mongoose = require('mongoose');
 
 const provision = async () => {
 
+    mongoose.connect('mongodb://localhost:27017/test');
+
+    const server = new Hapi.Server({
+        host: 'localhost',
+        port: 3000,
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    });
+
     await server.register(Inert);
+
+    await server.register({
+        plugin: require('hapi-mongodb'),
+        options: dbOpts
+    });
 
     server.route({
         method: 'GET',
