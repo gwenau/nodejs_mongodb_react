133ea0114ce4a516f524d9fd0919f56f0a6c1e2d
Gwen Au <quyen_au@hotmail.com>
2018-03-05 12:52:40 +1100

    hello.html page is loading now

--- server.js
+++ server.js
@@ -1,43 +1,34 @@
 'use strict';
 
-const Hapi=require('hapi');
+const Path = require('path');
+const Hapi = require('hapi');
+const Inert = require('inert');
 
-// Create a server with a host and port
-const server=Hapi.server({
-    host:'localhost',
-    port:3000
-});
-
-// Add the route
-server.route({
-    method:'GET',
-    path:'/hello',
-    handler:function(request,h) {
-
-        return'hello world';
+const server = new Hapi.Server({
+    port: 3000,
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, 'public')
+        }
     }
 });
 
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request) {  
-        return('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
+const provision = async () => {
 
-// Start the server
-async function start() {
+    await server.register(Inert);
 
-    try {
-        await server.start();
-    }
-    catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
+    server.route({
+        method: 'GET',
+        path: '/{path*}',
+        handler: {
+            file: 'hello.html'
+        }
+    });
+
+    await server.start();
 
     console.log('Server running at:', server.info.uri);
 };
 
-start();
+provision();
+
